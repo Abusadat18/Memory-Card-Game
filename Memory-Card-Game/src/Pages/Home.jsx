6225b5bdd2167fa6a2695049a6c624c1f594b5b9
Abu Sadat Ansari <itssadatansari@gmail.com>
2025-10-03 15:55:12 +0530

    Added the shuffle logic

--- Memory-Card-Game/src/Pages/Home.jsx
+++ Memory-Card-Game/src/Pages/Home.jsx
@@ -6,15 +6,27 @@ export function Home() {
   useEffect(() => {
     async function getCatsInfo() {
       const response = await fetch(
-        "https://api.giphy.com/v1/gifs/search?api_key=&q=cat&limit=15&offset=0&rating=g&lang=en&bundle=messaging_non_clips",
+        "https://api.giphy.com/v1/gifs/search?api_key=lHewHPbDqLntMxWfLpxMDHm5ROyebhjC&q=cat&limit=15&offset=0&rating=g&lang=en&bundle=messaging_non_clips",
       );
       const data = await response.json();
-      console.log(data.data); // returns an array of object
-      setCatInfo(data.data);
+      setCatInfo(data.data); // returns an array of objects
     }
 
     getCatsInfo();
   }, []);
+
+  // Shuffle cat info array using Fisher Yates algo
+  function shuffleCatInfo() {
+    const shuffledArray = [...catInfo];
+    for (let i = shuffledArray.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [shuffledArray[i], shuffledArray[j]] = [
+        shuffledArray[j],
+        shuffledArray[i],
+      ];
+    }
+    setCatInfo(shuffledArray);
+  }
 
   return (
     <>
@@ -30,6 +42,7 @@ export function Home() {
             <div // Card
               key={item.id}
               className="grid h-[140px] w-[100px] grid-rows-6 overflow-hidden rounded-xl border border-red-600 bg-[#d4a373] text-[#fefae0] md:h-[300px] md:w-[230px]"
+              onClick={shuffleCatInfo}
             >
               <div className="row-span-5">
                 <img
--- learning.md
+++ learning.md
@@ -4,3 +4,4 @@
 - Whenever we import a file, the file first runs from the top before it is been exported
 - Usage of object-fit property
 - In many places Grid solves a problem way easier than flexbox
+- Usage of event listeners in React, we pass the function (if without parameter) directly to onClick, but with parameters, we pass with a callback function.
